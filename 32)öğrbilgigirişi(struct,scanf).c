b48fa0c6bd6ae5d9f66a9a210364117aa29fdf7f
Burcu <64483622+burcuaslan@users.noreply.github.com>
2020-07-01 22:59:42 +0300

    Create 32)öğrbilgigirişi(struct,scanf).c

---
+++ 32)öğrbilgigirişi(struct,scanf).c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+struct ogrencibilgi 
+{
+	char ad[20];
+	int no;
+	float ort;
+};
+
+
+int main() {
+
+	struct ogrencibilgi ob;
+
+	printf("Ad Soyad: ");
+	scanf("%s", ob.ad);
+	printf("Numara: ");
+	scanf("%d", &ob.no);
+	printf("Ortalama: ");
+	scanf("%f", &ob.ort);
+
+	printf("\n*****Bilgileriniz*****\n");
+	printf("Adiniz: %s\n", ob.ad);
+	printf("Numaraniz: %d\n", ob.no);
+	printf("Ortalamaniz: %f", ob.ort);
+
+	return 0;
+}
