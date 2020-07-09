a014b7ee87ec8a0a5544cc22cad81c6f2d3c88ac
Burcu <64483622+burcuaslan@users.noreply.github.com>
2020-07-10 00:40:23 +0300

    Create 33)değişkenadıdeğiştirme(typedef).c

---
+++ 33)değişkenadıdeğiştirme(typedef).c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+
+	typedef int tamsayi;
+
+	tamsayi a;
+	a = 2;
+
+	tamsayi b, c;
+	b = 8;
+	c = 11;
+
+	tamsayi d = 7;
+
+	printf("%d %d %d %d", a, b, c, d);
+
+	return 0;
+}
