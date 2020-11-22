c931c742e3ea4eefe196dcb103f4367628d73816
Burcu <64483622+burcuaslan@users.noreply.github.com>
2020-11-22 14:45:20 +0300

    Update 10)faktoriyelhesaplama(while).c

--- 10)faktoriyelhesaplama(while).c
+++ 10)faktoriyelhesaplama(while).c
@@ -1,22 +1,19 @@
 #include <stdio.h>
 
 int main() {
-
 	
 	int fac;
 
 	printf("Faktoriyelini bulmak istediginiz sayiyi giriniz:");
 	scanf("%d", &fac);
 	
-  int i = (fac-1);
+	int i = (fac-1);
 	
 	while (1<i) {
-
 		fac = fac * i;
 		i--;
 	}
 
 	printf("Sonuc: %d", fac);
-
 	return 0;
 }
